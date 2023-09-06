2f3af67f4555a60a164eba879fac5c7673445465
Angela de Araujo <angela.araujo.rj@gmail.com>
2023-09-06 15:12:39 -0300

    Adicionando funcionalidades e relatorio de cobertura de teste

--- src/main/java/acc/br/HeroController.java
+++ src/main/java/acc/br/HeroController.java
@@ -8,21 +8,27 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.jboss.logging.Logger;
+
 import acc.br.model.Hero;
 import acc.br.service.HeroService;
 
 @Path("/hero")
-
 public class HeroController {
+
+    private static final Logger log = Logger.getLogger(HeroController.class);
+
     @Inject HeroService service;
     
     @GET
     public List<Hero> list() {
+        log.info("*** GET /hero");
         return service.getHeros();
     }
     
     @POST
-    public Response create(Hero hero) {        
+    public Response create(Hero hero) {    
+        log.info("*** POST /hero");    
         service.add(hero);
         return Response.ok(hero).status(201).build();
     }
--- src/test/java/acc/br/HeroControllerTest.java
+++ src/test/java/acc/br/HeroControllerTest.java
@@ -2,12 +2,9 @@ package acc.br;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-import acc.br.HeroController;
 import acc.br.service.HeroService;
 
 import static io.restassured.RestAssured.given;
@@ -16,66 +13,57 @@ import static org.hamcrest.CoreMatchers.is;
 import javax.inject.Inject;
 
 import static org.hamcrest.CoreMatchers.containsString;
- 
+
 @QuarkusTest
 @TestHTTPEndpoint(HeroController.class)
-
 public class HeroControllerTest {
 
-	@Test 
+	@Test
 	public void testSize() {
-
 		given()
-		.when().get()
-		.then()
-		.statusCode(200);
-
+				.when().get()
+				.then()
+					.statusCode(200);
 	}
 
 	@Test
 	public void testBody() {
-
 		given()
-		.when().get()
-		.then()
-		.statusCode(200)
-		.body(
-				containsString("\"name\":\"Bruce\",\"surname\":\"Wayne\""),
-				containsString("\"name\":\"Peter\",\"surname\":\"Parker\""));
-
-
-
+				.when().get()
+				.then()
+				.statusCode(200)
+				.body(
+						containsString("\"name\":\"Bruce\",\"surname\":\"Wayne\""),
+						containsString("\"name\":\"Peter\",\"surname\":\"Parker\""));
 	}
 
 	@Test
 	public void testPost() {
 		given()
-		.body("{\"name\": \"Bruce\", \"surname\": \"Banner\"}")
-		.header("Content-Type", "application/json")
-		.when()
-		.post()
-		.then()
-		.statusCode(201);
+				.body("{\"name\": \"Bruce\", \"surname\": \"Banner\"}")
+				.header("Content-Type", "application/json")
+				.when()
+				.post()
+				.then()
+				.statusCode(201);
 
 		given()
-		.when().get()
-		.then()
-		.statusCode(200)
-		.body("$.size()", is(3),
-				"[0].name", is("Bruce"),
-				"[0].surname", is("Wayne"),
-				"[1].name", is("Peter"),
-				"[1].surname", is("Parker"),
-				"[2].name", is("Bruce"),
-				"[2].surname", is("Banner"));
+				.when().get()
+				.then()
+				.statusCode(200)
+				.body("$.size()", is(3),
+						"[0].name", is("Bruce"),
+						"[0].surname", is("Wayne"),
+						"[1].name", is("Peter"),
+						"[1].surname", is("Parker"),
+						"[2].name", is("Bruce"),
+						"[2].surname", is("Banner"));
 	}
 
 	@Inject HeroService service;
 	@Test
-	public void testService() {
-
+	public void testService() {		
 		int heros = service.getHeros().size();
-		assertEquals(heros, 2);
-
+		Assertions.assertEquals(heros, 2);
 	}
 }
